1cd00deec54e9400b0a603c6eece83114bf28dfd
meitoc <info@meitoc.com>
2023-06-21 20:34:48 +0700

    counter

--- src/formatTime.js
+++ src/formatTime.js
@@ -3,7 +3,7 @@ export const formatTime = (time) => {
   let seconds = Math.floor(time/1000)
   const getSeconds = String(seconds%60).padStart(2, '0');
   // const minutes = Math.floor(time/60);
-  const getMinutes = String(Math.floor(seconds/60)).padStart(2, '0');;
+  const getMinutes = String(Math.floor(seconds/60)%60).padStart(2, '0');;
   const getHours = String(Math.floor(seconds/3600)).padStart(2, '0');
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
