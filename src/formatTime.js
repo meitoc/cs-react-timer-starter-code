ba173a29a9a9f8273207a15d10602d1a99348995
meitoc <info@meitoc.com>
2023-06-21 16:29:44 +0700

    counter

--- src/formatTime.js
+++ src/formatTime.js
@@ -1,8 +1,10 @@
 export const formatTime = (time) => {
-  const getSeconds = "Your code here";
-  const minutes = "Your code here";
-  const getMinutes = "Your code here";
-  const getHours = "Your code here";
+  if(time===null) time=0;
+  let seconds = Math.floor(time/1000)
+  const getSeconds = String(seconds%60).padStart(2, '0');
+  // const minutes = Math.floor(time/60);
+  const getMinutes = String(Math.floor(seconds/60)).padStart(2, '0');;
+  const getHours = String(Math.floor(seconds/3600)).padStart(2, '0');
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,57 @@
-import { useState, useRef } from "react";
+import { useState, useRef} from "react";
 
-const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+const useTimer = (ini = 0) => {//custom hook
+  const [time, setTime] = useState(null);//"Your code here";
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(null);//"Your code here";
+  const active = useRef(false);
+  const refInterval =  useRef(null);//"Your code here";
+  const endTime =  useRef(0);//"Your code here";
 
   const startTimer = () => {
-    "Your code here";
-    active.current.disabled = true;
+    // "Your code here";
+    if(isStart.current===null){
+      isStart.current= new Date().getTime();
+      active.current.disabled = true;
+      // setTime(endTime.current);
+      refInterval.current = setInterval(()=>{
+        if(isStart.current!==null){
+          let realTime = new Date().getTime();
+          endTime.current += (realTime - isStart.current);
+          isStart.current = realTime;
+          setTime(endTime.current);
+          console.log(endTime.current);
+        }
+        else clearInterval(this);
+      },1000);
+    }
   };
   const stopTimer = () => {
-    "Your code here";
+    // "Your code here";
+    //clear interval:
+    clearInterval(refInterval.current);
+    refInterval.current=null;
+    
+    if(isStart.current!==null){
+      let realTime = new Date().getTime();
+      endTime.current += (realTime - isStart.current);
+      isStart.current=null;
+      setTime(endTime.current);
+      console.log(endTime.current)
+      active.current.disabled=false;
+    }
   };
   const resetTimer = () => {
-    "Your code here";
+    // "Your code here";
+    //clear interval:
+    clearInterval(refInterval.current);
+    refInterval.current=null;
+
+    isStart.current=null;
+    endTime.current=0;
+    setTime(null);
     active.current.disabled = false;
   };
-
   return { time, startTimer, stopTimer, resetTimer, active };
 };
 export default useTimer;
